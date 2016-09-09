9652ed9c77b9a630f25fe2cb96811c7bf31cc03f
wangda <11951691@qq.com>
2016-09-09 15:17:42 +0800

    Initial Commit

---
+++ CloudSchool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudSchool
+//
+//  Created by 汪达 on 16/9/9.
+//  Copyright © 2016年 wd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CloudSchool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudSchool
+//
+//  Created by 汪达 on 16/9/9.
+//  Copyright © 2016年 wd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
